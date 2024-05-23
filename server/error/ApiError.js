97a1a158eb2f803384962385b6af5bbeae132257
NikiDan <novakhudonosor@mail.ru>
2024-05-23 21:59:09 +0300

    Errors, Middleware, Controllers

---
+++ server/error/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+	constructor(message, status) {
+		super()
+		this.message = message
+		this.status = status
+	}
+
+	static badRequest(message) {
+		return new ApiError(message, 404)
+	}
+
+	static internal(message) {
+		return new ApiError(message, 500)
+	}
+
+	static forbidden(message) {
+		return new ApiError(message, 403)
+	}
+}
+
+module.exports = ApiError
